a897faa1aaad96e56e713c8c890baa05ccb1686e
Lyubomir Dias <lyubomirdias@gmail.com>
2021-06-30 15:05:04 +0100

    styled page

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -5,18 +5,18 @@ import { dijkstra, getNodesInShortestPathOrder } from "../algorithms/dijkstra";
 import "./PathfindingVisualizer.scss";
 
 const START_NODE_ROW = 10;
-const START_NODE_COL = 15;
-const FINISH_NODE_ROW = 10;
-const FINISH_NODE_COL = 35;
+const START_NODE_COL = 11;
+const FINISH_NODE_ROW = 37;
+const FINISH_NODE_COL = 17;
 
-// export default class PathfindingVisualizer extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       grid: [],
-//       mouseIsPressed: false,
-//     };
-//   }
+export default class PathfindingVisualizer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      grid: [],
+      mouseIsPressed: false,
+    };
+  }
 
   componentDidMount() {
     const grid = getInitialGrid();
@@ -43,14 +43,14 @@ const FINISH_NODE_COL = 35;
       if (i === visitedNodesInOrder.length) {
         setTimeout(() => {
           this.animateShortestPath(nodesInShortestPathOrder);
-        }, 10 * i);
+        }, 5 * i);
         return;
       }
       setTimeout(() => {
         const node = visitedNodesInOrder[i];
         document.getElementById(`node-${node.row}-${node.col}`).className =
           "node node-visited";
-      }, 10 * i);
+      }, 5 * i);
     }
   }
 
@@ -60,7 +60,7 @@ const FINISH_NODE_COL = 35;
         const node = nodesInShortestPathOrder[i];
         document.getElementById(`node-${node.row}-${node.col}`).className =
           "node node-shortest-path";
-      }, 50 * i);
+      }, 40 * i);
     }
   }
 
@@ -78,8 +78,8 @@ const FINISH_NODE_COL = 35;
 
     return (
       <>
-        <button onClick={() => this.visualizeDijkstra()}>
-          Visualize Dijkstra's Algorithm
+        <button onClick={() => this.visualizeDijkstra()} className="button">
+          Get the money pot!
         </button>
         <div className="grid">
           {grid.map((row, rowIdx) => {
@@ -115,9 +115,9 @@ const FINISH_NODE_COL = 35;
 
 const getInitialGrid = () => {
   const grid = [];
-  for (let row = 0; row < 20; row++) {
+  for (let row = 0; row < 45; row++) {
     const currentRow = [];
-    for (let col = 0; col < 50; col++) {
+    for (let col = 0; col < 25; col++) {
       currentRow.push(createNode(col, row));
     }
     grid.push(currentRow);
